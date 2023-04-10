3a9b2bcd41b1c3ec0079c02116b5b5a0b30f97e3
Matheus Falcão <37125090+UnlockTutors@users.noreply.github.com>
2023-04-10 11:38:50 -0300

    Add files via upload

--- projeto git simulation/src/main.c
+++ projeto git simulation/src/main.c
@@ -1,30 +1,26 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "git_commands.h"
-#include "data_structures.h"
 
 int main() {
-    char input[100];
     Branch *branches = NULL;
     Branch *current_branch = NULL;
 
-    while (1) {
-        printf("> ");
-        fgets(input, sizeof(input), stdin);
-        input[strcspn(input, "\n")] = '\0';  // Remover a quebra de linha
+    // Inicializando o branch master
+    git_branch(&branches, "master");
+    current_branch = branches;
 
-        if (strncmp(input, "git commit -m ", 14) == 0) {
-            git_commit(current_branch, input + 14);
-        } else if (strncmp(input, "git branch ", 11) == 0) {
-            git_branch(&branches, input + 11);
-        } else if (strncmp(input, "git checkout ", 13) == 0) {
-            git_checkout(&current_branch, branches, input + 13);
-        } else if (strncmp(input, "git merge ", 10) == 0) {
-            git_merge(current_branch, branches, input + 10);
-        } else {
-            printf("Comando não reconhecido: %s\n", input);
-        }
-    }
+    // Simulando alguns comandos git
+    git_commit(current_branch, "Initial commit");
+    git_branch(&branches, "feature1");
+    git_checkout(&current_branch, branches, "feature1");
+    git_commit(current_branch, "Add feature 1");
+    git_checkout(&current_branch, branches, "master");
+    git_merge(current_branch, branches, "feature1");
+
+    // Adicione mais comandos aqui para testar a simulação
+
+    // Lembre-se de liberar a memória alocada para branches e commits antes de sair do programa
 
     return 0;
 }
